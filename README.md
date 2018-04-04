f8a653b300b592966ceaccaae5a03ea978e038fe
Jingbei Li <i@jingbei.li>
2018-04-04 16:41:12 +0800

    added PushBear support

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 python_serverchan
 ====
-Tiny python library for [ServerChan](http://sc.ftqq.com/3.version)
+Tiny python library for [ServerChan](http://sc.ftqq.com/3.version) and [PushBear](https://pushbear.ftqq.com/admin/#/)
 
 ### Dependencies
 * Python 3
@@ -13,9 +13,19 @@ $ git clone https://github.com/petronny/python_serverchan
 ```
 
 ### Usage
+
+* ServerChan
 ```python
 from python_serverchan import ServerChan
 
 serverchan = ServerChan('your_serverchan_key')
 serverchan.send('title', 'body')
+```
+
+* PushBear
+```python
+from python_serverchan import PushBear
+
+pushbear = PushBear('your_send_key')
+pushbear.send('title', 'body')
 ```
--- __init__.py
+++ __init__.py
@@ -4,12 +4,22 @@ import urllib.parse
 import json
 
 class ServerChan:
-
-    SERVERCHAN_URL = 'https://sc.ftqq.com/%s.send?%s'
+    API_URL = 'https://sc.ftqq.com/%s.send?%s'
 
     def __init__(self, serverchan_key):
         self.serverchan_key = serverchan_key
 
     def send(self, title, body):
-        url = ServerChan.SERVERCHAN_URL % (self.serverchan_key , urllib.parse.urlencode(dict(text=title, desp=body)))
+        url = ServerChan.API_URL % (self.serverchan_key , urllib.parse.urlencode(dict(text=title, desp=body)))
+        return json.loads(urllib.request.urlopen(url).read().decode('utf-8'))
+
+class PushBear:
+
+    API_URL = 'https://pushbear.ftqq.com/sub?%s'
+
+    def __init__(self, send_key):
+        self.send_key = send_key
+
+    def send(self, title, body):
+        url = PushBear.API_URL % (urllib.parse.urlencode(dict(sendkey=self.send_key, text=title, desp=body)))
         return json.loads(urllib.request.urlopen(url).read().decode('utf-8'))
